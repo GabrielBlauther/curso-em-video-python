6f0b2919e1b95e2ffad29e7e0428b906110d61f3
Gabriel Blauther <gblauther@gmail.com>
2026-09-22 18:02:12 -0300

    Feat: Exercice

---
+++ modules/exercice.py
@@ -0,0 +1,8 @@
+from utils import fatorial, dobro, triplo
+num = int(input("Digite um número: "))
+fat = fatorial(num)
+dob = dobro(num)
+tri = triplo(num)
+print(f"O fatorial de {num} é {fat}.")  
+print(f"O dobro de {num} é {dob}.")
+print(f"O triplo de {num} é {tri}.")
